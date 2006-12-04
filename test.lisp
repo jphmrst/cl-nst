7b768429a630177ec6c46932f9d4ab2346a5c9ef
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2006-12-04 17:12:16 +0000

    checkpoint

--- test.lisp
+++ test.lisp
@@ -1,36 +1,60 @@
-:als nst
-:pa nst
 
-(def-fixtures f1 ()
-  ((c 3)
-   (d 'asdfg))
-  ())
+(defgeneric wrap (o)
+  (:method (o) (format t "core~%")))
 
-(def-fixtures f1 ()
-  ((c 4)
-   (d 'asdfg))
-  ())
+(defclass c1 () ())
+(defmethod wrap :around ((o c1))
+  (format t "c1 wrap~%")
+  (call-next-method)
+  (format t "c1 unwrap~%"))
 
-(def-test-group g1 (f1)
-  (def-test t1 :form (eql 1 1))
-  (def-test t2 :form (eql 1 2))
-  (def-test t3 :form (eql 2 2))
-  (def-test t4 :form (eql c 3))
-  (def-test t5 :form (eq d 'asdfg)))
+(defclass c2 () ())
+(defmethod wrap :around ((o c2))
+  (format t "c2 wrap~%")
+  (call-next-method)
+  (format t "c2 unwrap~%"))
 
-(def-test-group g1 (f1)
-  (def-test t1 :form (eql 1 1))
-  (def-test t3 :form (eql 2 2))
-  (def-test t7 :form (eq d 'asdfg))
-  (def-test t8 :form (eql c 4)))
+(defclass cc (c1 c2) ())
+(defparameter cco (make-instance 'cc))
 
-:nst :dump
+(defclass cc (c1 c2) ())
+(defparameter cco (make-instance 'cc))
 
-:nst :run-test g1 t1
+------------------------------------------------------------
 
-:nst :run-group g1
-
-:nst help
+;;;:als nst
+;;;:pa nst
+;;;
+;;;(def-fixtures f1 ()
+;;;  ((c 3)
+;;;   (d 'asdfg))
+;;;  ())
+;;;
+;;;(def-fixtures f1 ()
+;;;  ((c 4)
+;;;   (d 'asdfg))
+;;;  ())
+;;;
+;;;(def-test-group g1 (f1)
+;;;  (def-test t1 :form (eql 1 1))
+;;;  (def-test t2 :form (eql 1 2))
+;;;  (def-test t3 :form (eql 2 2))
+;;;  (def-test t4 :form (eql c 3))
+;;;  (def-test t5 :form (eq d 'asdfg)))
+;;;
+;;;(def-test-group g1 (f1)
+;;;  (def-test t1 :form (eql 1 1))
+;;;  (def-test t3 :form (eql 2 2))
+;;;  (def-test t7 :form (eq d 'asdfg))
+;;;  (def-test t8 :form (eql c 4)))
+;;;
+;;;:nst :dump
+;;;
+;;;:nst :run-test g1 t1
+;;;
+;;;:nst :run-group g1
+;;;
+;;;:nst help
 
 ------------------------------------------------------------
 
