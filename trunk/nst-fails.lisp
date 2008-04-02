8ccdf2d0f41921c24a4a7d1dbf2e9ac6654aa3df
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2008-04-02 17:01:00 +0000

    license info for NST --- lesser

--- trunk/nst-fails.lisp
+++ trunk/nst-fails.lisp
@@ -6,17 +6,18 @@
 ;;; Derived from RRT, Copyright (c) 2005 Robert Goldman.
 ;;;
 ;;; NST is free software: you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation, either version 3 of the License, or
-;;; (at your option) any later version.
+;;; under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
 ;;;
 ;;; NST is distributed in the hope that it will be useful, but WITHOUT
 ;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-;;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-;;; License for more details.
+;;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+;;; Public License for more details.
 ;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with NST.  If not, see <http://www.gnu.org/licenses/>.
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with NST.  If not, see
+;;; <http://www.gnu.org/licenses/>.
 (in-package :nst-interact)
 
 ;;; Some examples of failing tests.
