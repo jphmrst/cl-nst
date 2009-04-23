a9f95b68063a37b452695609a74eb9fb28f35bac
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2009-04-23 19:45:48 +0000

    Our ticket:69 and (at least a first try at) BBN ticket poirot:ticket:439

--- trunk/core/junit.lisp
+++ trunk/core/junit.lisp
@@ -30,51 +30,51 @@
 argument should be a string of just spaces."))
 
 (defmethod junit-xml-snippet ((item multi-results)
-			      &optional (s *standard-output*) (padding ""))
+                              &optional (s *standard-output*) (padding ""))
   (with-accessors ((elapsed-time result-stats-elapsed-time)
-		   (tests result-stats-tests)
-		   (errors result-stats-erring)
-		   (failures result-stats-failing)
-		   (system multi-results-system)
-		   (group-reports multi-results-group-reports)) item
+                   (tests result-stats-tests)
+                   (errors result-stats-erring)
+                   (failures result-stats-failing)
+                   (system multi-results-system)
+                   (group-reports multi-results-group-reports)) item
     (let ((new-padding (concatenate 'string "  " padding)))
       (loop for report in group-reports do
-	(cond (report (junit-xml-snippet report s new-padding)))))))
+        (cond (report (junit-xml-snippet report s new-padding)))))))
 
 (defmethod junit-xml-snippet ((item group-result)
-			      &optional (s *standard-output*) (padding ""))
+                              &optional (s *standard-output*) (padding ""))
   (with-accessors ((elapsed-time result-stats-elapsed-time)
-		   (tests result-stats-tests)
-		   (errors result-stats-erring)
-		   (failures result-stats-failing)
-		   (system multi-results-system)
-		   (group-name group-result-group-name)
-		   (test-reports group-result-check-results)
-		   (timestamp result-stats-timestamp)) item
+                   (tests result-stats-tests)
+                   (errors result-stats-erring)
+                   (failures result-stats-failing)
+                   (system multi-results-system)
+                   (group-name group-result-group-name)
+                   (test-reports group-result-check-results)
+                   (timestamp result-stats-timestamp)) item
     (destructuring-bind (second minute hour date month year day daylight-p zone)
-	timestamp
+        timestamp
       (declare (ignorable day daylight-p zone))
       (format s
-	  "~a<testsuite~@< errors=\"~d\"~
+          "~a<testsuite~@< errors=\"~d\"~
                      ~:_ failures=\"~d\"~
                      ~:_ name=~s~
                      ~:_ tests=\"~d\"~
                      ~:_ time=\"~f\"~
                      ~:_ timestamp=\"~d-~2,'0d-~2,'0dT~2,'0d:~2,'0d:~2,'0d\"~
                   ~@[~:_ hostname=~s~]~:>>~%"
-	padding errors failures (symbol-to-junit-name group-name) tests
-	(/ elapsed-time internal-time-units-per-second)
-	year month date hour minute second
-      
-	;; The hostname.  This isn't Lisp-standard, so maybe we can't
-	;; have it.
-	#+allegro (let ((outputs (excl.osi:command-output "hostname")))
-		    (if (and outputs (stringp (car outputs)))
-		      (car outputs)))
-	#-allegro nil)
+        padding errors failures (symbol-to-junit-name group-name) tests
+        (/ elapsed-time internal-time-units-per-second)
+        year month date hour minute second
+
+        ;; The hostname.  This isn't Lisp-standard, so maybe we can't
+        ;; have it.
+        #+allegro (let ((outputs (excl.osi:command-output "hostname")))
+                    (if (and outputs (stringp (car outputs)))
+                      (car outputs)))
+        #-allegro nil)
       (let ((new-padding (concatenate 'string "  " padding)))
-	(loop for report being the hash-values of test-reports do
-	      (cond (report (junit-xml-snippet report s new-padding)))))
+        (loop for report being the hash-values of test-reports do
+              (cond (report (junit-xml-snippet report s new-padding)))))
       (format s "~a  <system-out><![CDATA[" padding)
       (nst-dump :stream s)
       (format s "]]></system-out>~%")
@@ -82,44 +82,55 @@ argument should be a string of just spaces."))
       (format s "~a</testsuite>~%" padding))))
 
 (defmethod junit-xml-snippet ((item check-result)
-			      &optional (s *standard-output*) (padding ""))
+                              &optional (s *standard-output*) (padding ""))
   (with-accessors ((group-name check-result-group-name)
-		   (check-name check-result-check-name)
-		   (warnings check-result-warnings)
-		   (failures check-result-failures)
-		   (errors check-result-errors)
-		   (info check-result-info)
-		   (elapsed-time check-result-elapsed-time)
-		   (timestamp result-stats-timestamp)) item
+                   (check-name check-result-check-name)
+                   (warnings check-result-warnings)
+                   (failures check-result-failures)
+                   (errors check-result-errors)
+                   (info check-result-info)
+                   (elapsed-time check-result-elapsed-time)
+                   (timestamp result-stats-timestamp)) item
     (destructuring-bind (second minute hour date month year day daylight-p zone)
-	timestamp
+        timestamp
       (declare (ignorable day daylight-p zone))
       (format s "~a<testcase classname=~s name=\"~a\" time=\"~f\" ~
                       timestamp=\"~4,'0d-~2,'0d-~2,'0dT~2,'0d:~2,'0d:~2,'0d\""
-	padding
-	(symbol-to-junit-name group-name) ; use the group for the classname
-	check-name (/ elapsed-time internal-time-units-per-second)
-	year month date hour minute second)
+        padding
+        (symbol-to-junit-name group-name) ; use the group for the classname
+        check-name (/ elapsed-time internal-time-units-per-second)
+        year month date hour minute second)
       (cond
-	(errors
-	 (format s
-	     ">~:{~%~a  ~@<<error message=\"~a raised an error: ~a\" type=~s>~
+        (errors
+         (format s
+             ">~:{~%~a  ~@<<error message=\"~a raised an error: ~a\" type=~s>~
                          ~:@_  Lisp backtrace (within NST context):~
                          ~:@_    ~:[expression top-level~
                                   ~;~@<~:*~{~a~^~:@_~}~:>~]~
                          ~:@_</error>~:>~}~%"
-	   (loop for error-note in errors
-		 collect (with-accessors ((error error-check-note-error))
-			     error-note
-			   (list padding
-				 (symbol-to-junit-name check-name)
-				 (symbol-to-junit-name (type-of error))
-				 (symbol-to-junit-name (type-of error))
-				 #+allegro (error-check-note-zoom error-note)
-				 #-allegro nil))))
-	 (format s "~a</testcase>~%" padding))
-       (failures (format s ">~%~a  <failure type=\"FAILURE\"/>~%" padding)
-		 (format s "~a</testcase>~%" padding))
+           (loop for error-note in errors
+                 collect (with-accessors ((error error-check-note-error))
+                             error-note
+                           (list padding
+                                 (symbol-to-junit-name check-name)
+                                 (symbol-to-junit-name (type-of error))
+                                 (symbol-to-junit-name (type-of error))
+                                 #+allegro (error-check-note-zoom error-note)
+                                 #-allegro nil))))
+         (format s "~a</testcase>~%" padding))
+        (failures (with-accessors ((context check-note-context)
+                                   (stack check-note-stack)
+                                   (format check-note-format)
+                                   (args check-note-args))
+                      (car failures)
+                    (format s
+                        ">~%~a  <failure message=\"~?\" type=\"lisp.nst.criterion.~a\"><![CDATA[~%"
+                      padding format args
+                      (context-layer-criterion (car context)))
+                    (format s "~a    ~@<~{~a~^~:@_~}~:>~%"
+                      padding context))
+                  (format s "~a  ]]></failure>~%" padding)
+                  (format s "~a</testcase>~%" padding))
        (t (format s " />~%"))))))
 
 (defun nst-xml-dump (stream)
@@ -141,77 +152,77 @@ argument should be a string of just spaces."))
 
 (defmethod nst-junit-dump ((filename string))
   (with-open-file (stream filename :if-exists :supersede
-			  :if-does-not-exist :create)
+                          :if-does-not-exist :create)
     (nst-junit-dump stream)))
 
 (defgeneric junit-group-result (group &rest args &key
-				dir file verbose
-				if-dir-does-not-exist if-file-exists))
+                                dir file verbose
+                                if-dir-does-not-exist if-file-exists))
 
 (defmethod junit-group-result ((group symbol) &rest args &key
-			       dir file verbose
-			       if-dir-does-not-exist if-file-exists)
+                               dir file verbose
+                               if-dir-does-not-exist if-file-exists)
   (declare (ignorable dir file if-dir-does-not-exist if-file-exists)
            (ignore verbose))
   (apply #'junit-group-result (group-report group) args))
 
 (defmethod junit-group-result ((group group-result) &rest args &key
-			       verbose
-			       (dir  nil dir-supp-p)
-			       (file nil file-supp-p)
-			       (stream nil stream-supp-p)
-			       (if-dir-does-not-exist
-				:create if-dir-does-not-exist-supp-p)
-			       (if-file-exists :new-version
-					       if-file-exists-supp-p))
+                               verbose
+                               (dir  nil dir-supp-p)
+                               (file nil file-supp-p)
+                               (stream nil stream-supp-p)
+                               (if-dir-does-not-exist
+                                :create if-dir-does-not-exist-supp-p)
+                               (if-file-exists :new-version
+                                               if-file-exists-supp-p))
   (declare (ignorable args))
 
   (when (and stream-supp-p (or dir-supp-p file-supp-p))
     (warn "Using :stream, ignoring :directory/:filename."))
   (when (and stream-supp-p (or if-dir-does-not-exist-supp-p
-			       if-file-exists-supp-p))
+                               if-file-exists-supp-p))
     (warn "Options :if-dir-does-not-exist, :if-file-exists not used with :stream, ignoring"))
-    
+
   (let ((the-stream
-	 (cond
-	  (stream-supp-p stream)
-	     
-	  ((or dir-supp-p file-supp-p)
-  
-	   ;; If there's no filename, build a default one.
-	   (when (not file-supp-p)
-	     (setf file (parse-namestring (concatenate 'string
-					    "TEST-"
-					    (symbol-to-junit-name
-					     (group-result-group-name group))
-					    ".xml"))))
-  
-	   ;; If there's no directory, use the current one.
-	   (when (not dir-supp-p)
-	     (setf dir (parse-namestring "./")))
-  
-	   ;; If we just have a string for the filename, convert it to
-	   ;; a Lisp pathname.
-	   (when (and file-supp-p (stringp file))
-	     (setf file (parse-namestring file)))
-  
-	   ;; If we just have a string for the directory, convert it
-	   ;; to a Lisp pathname.
-	   (when (and dir-supp-p (stringp dir))
-	     (setf dir (parse-namestring dir)))
+         (cond
+          (stream-supp-p stream)
 
-	   (let ((file-path (merge-pathnames file dir)))
+          ((or dir-supp-p file-supp-p)
 
-	     (when (eq if-dir-does-not-exist :create)
-	       (ensure-directories-exist file-path))
-	     
-	     (when verbose (format t "  Writing to ~w~%" file-path))
-	     (open file-path :direction :output :element-type :default
-		   :if-exists if-file-exists :if-does-not-exist :create)))
-	  
-	  (t (setf stream-supp-p t)
-	     *standard-output*))))
-    
+           ;; If there's no filename, build a default one.
+           (when (not file-supp-p)
+             (setf file (parse-namestring (concatenate 'string
+                                            "TEST-"
+                                            (symbol-to-junit-name
+                                             (group-result-group-name group))
+                                            ".xml"))))
+
+           ;; If there's no directory, use the current one.
+           (when (not dir-supp-p)
+             (setf dir (parse-namestring "./")))
+
+           ;; If we just have a string for the filename, convert it to
+           ;; a Lisp pathname.
+           (when (and file-supp-p (stringp file))
+             (setf file (parse-namestring file)))
+
+           ;; If we just have a string for the directory, convert it
+           ;; to a Lisp pathname.
+           (when (and dir-supp-p (stringp dir))
+             (setf dir (parse-namestring dir)))
+
+           (let ((file-path (merge-pathnames file dir)))
+
+             (when (eq if-dir-does-not-exist :create)
+               (ensure-directories-exist file-path))
+
+             (when verbose (format t "  Writing to ~w~%" file-path))
+             (open file-path :direction :output :element-type :default
+                   :if-exists if-file-exists :if-does-not-exist :create)))
+
+          (t (setf stream-supp-p t)
+             *standard-output*))))
+
     (junit-header the-stream)
     (junit-xml-snippet group the-stream)
     (unless stream-supp-p (close the-stream))))
