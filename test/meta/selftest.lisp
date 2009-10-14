5c94deaa728592be28c332fb617dde7dccdd2aeb
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2009-10-14 03:15:43 +0000

    metacriteria

--- test/meta/selftest.lisp
+++ test/meta/selftest.lisp
@@ -2,4 +2,6 @@
 (in-package :mnst)
 
 (def-test-group m-a-setup-cleanup ()
-  (def-test runner (--nst-group a-setup-cleanup :true) nil))
+  (def-test runner (--nst-group mnst-src::simple-pass
+                     (---test-passes mnst-src::simple-pass mnst-src::sp))))
+
