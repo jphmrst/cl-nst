606a3fe10ee548c925e73877ec4b4a1fe6aa8249
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2009-11-11 23:14:41 +0000

    Add self-tests to check for correct translation of error arising from setup/cleanup forms into errors recorded for tests

--- test/meta/selftest.lisp
+++ test/meta/selftest.lisp
@@ -8,9 +8,37 @@
         (---test-errs mnst-src::boom-group-fixture mnst-src::bf2)
         (---test-errs mnst-src::boom-group-fixture mnst-src::bf3)
         (---test-errs mnst-src::boom-group-fixture mnst-src::bf4)))
+  (def-test group-setup-error
+      (--nst-group mnst-src::boom-group-setup
+        (---test-errs mnst-src::boom-group-setup mnst-src::bgs1)
+        (---test-errs mnst-src::boom-group-setup mnst-src::bgs2)
+        (---test-errs mnst-src::boom-group-setup mnst-src::bgs3)
+        (---test-errs mnst-src::boom-group-setup mnst-src::bgs4)))
+  (def-test group-cleanup-error
+      (--nst-group mnst-src::boom-group-cleanup
+        (---test-errs mnst-src::boom-group-cleanup mnst-src::bgc1)
+        (---test-errs mnst-src::boom-group-cleanup mnst-src::bgc2)
+        (---test-errs mnst-src::boom-group-cleanup mnst-src::bgc3)
+        (---test-errs mnst-src::boom-group-cleanup mnst-src::bgc4)))
+
   (def-test test-fixture-error
       (--nst-group mnst-src::boom-test-fixture
-        (---test-errs mnst-src::boom-test-fixture mnst-src::bf2))))
+        (---test-passes mnst-src::boom-test-fixture mnst-src::bf1)
+        (---test-errs mnst-src::boom-test-fixture mnst-src::bf2)
+        (---test-passes mnst-src::boom-test-fixture mnst-src::bf3)
+        (---test-passes mnst-src::boom-test-fixture mnst-src::bf4)))
+  (def-test test-setup-error
+      (--nst-group mnst-src::boom-test-setup
+        (---test-passes mnst-src::boom-test-setup mnst-src::bts1)
+        (---test-errs mnst-src::boom-test-setup mnst-src::bts2)
+        (---test-passes mnst-src::boom-test-setup mnst-src::bts3)
+        (---test-passes mnst-src::boom-test-setup mnst-src::bts4)))
+  (def-test test-cleanup-error
+      (--nst-group mnst-src::boom-test-cleanup
+        (---test-passes mnst-src::boom-test-cleanup mnst-src::btc1)
+        (---test-errs mnst-src::boom-test-cleanup mnst-src::btc2)
+        (---test-passes mnst-src::boom-test-cleanup mnst-src::btc3)
+        (---test-passes mnst-src::boom-test-cleanup mnst-src::btc4))))
 
 (def-test-group m-a-setup-cleanup ()
   (def-test runner (--nst-group mnst-src::simple-pass
