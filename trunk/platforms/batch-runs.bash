c53da21d2d16e346279abed615e6ba2905c8b221
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2009-11-12 04:35:27 +0000

    actually, batch-test both the trunk and branch

--- trunk/platforms/batch-runs.bash
+++ trunk/platforms/batch-runs.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 mkdir logs
-cat < /dev/null > logs/cmucl0.log
-cmucl -batch < cmucl.lisp 2>> logs/cmucl0.log >> logs/cmucl0.log
+
+echo > logs/cmucl1.log
+cmucl -batch < cmucl1.lisp 2>> logs/cmucl1.log >> logs/cmucl1.log
+
+echo > logs/cmucl0.log
+cmucl -batch < cmucl0.lisp 2>> logs/cmucl0.log >> logs/cmucl0.log
+
