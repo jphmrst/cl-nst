6767511580f5bbc3935fb213d2d06f3c9bc0c5c6
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2009-12-13 05:13:44 +0000

    some conditionals for ECL

--- platforms/batch-runs.bash
+++ platforms/batch-runs.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 mkdir logs
 
+echo Testing the branch on ECL
+echo > logs/ecl-branch.log
+ecl -load branch-pushes.lisp 2>> logs/ecl-branch.log >> logs/ecl-branch.log
+
+echo Testing the trunk on ECL
+echo > logs/ecl-trunk.log
+ecl -load trunk-pushes.lisp 2>> logs/ecl-trunk.log >> logs/ecl-trunk.log
+
 echo Testing the branch on CLISP
 echo > logs/clisp-branch.log
 clisp -i clisp-init.lisp branch-pushes.lisp 2>> logs/clisp-branch.log >> logs/clisp-branch.log
@@ -25,10 +33,10 @@ echo Testing the trunk on SBCL
 echo > logs/sbcl-trunk.log
 sbcl < trunk-asdfind.lisp 2>> logs/sbcl-trunk.log >> logs/sbcl-trunk.log
 
-echo Testing the branch on CMU CL
-echo > logs/cmucl-branch.log
-cmucl -batch < branch-asdfind.lisp 2>> logs/cmucl-branch.log >> logs/cmucl-branch.log
-
-echo Testing the trunk on CMU CL
-echo > logs/cmucl-trunk.log
-cmucl -batch < trunk-asdfind.lisp 2>> logs/cmucl-trunk.log >> logs/cmucl-trunk.log
+# echo Testing the branch on CMU CL
+# echo > logs/cmucl-branch.log
+# cmucl -batch < branch-asdfind.lisp 2>> logs/cmucl-branch.log >> logs/cmucl-branch.log
+# 
+# echo Testing the trunk on CMU CL
+# echo > logs/cmucl-trunk.log
+# cmucl -batch < trunk-asdfind.lisp 2>> logs/cmucl-trunk.log >> logs/cmucl-trunk.log
