96187e8eec4540cd29e2bf933deeba3d9f7e2bac
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-02-19 22:52:59 +0000

    Diagnostic tools for the JUnit stuff

---
+++ trunk/platforms/junit-writer.lisp
@@ -0,0 +1,14 @@
+(nstdev)
+(asdf:operate 'asdf:load-op :nst-simple-tests)
+
+(let ((*print-readably* nil))
+  (nst-control-api:run-package :nst-simple-tests))
+
+
+(let ((logdir
+       (merge-pathnames (#+allegro sys:getenv #+sbcl sb-ext:posix-getenv
+                                   #-(or allegro sbcl) error
+                                   "NSTJUNITDIR")
+                        (make-pathname :directory (pathname-directory "./")))))
+  (format t "~a~%" logdir)
+  (nst:junit-results-by-group :dir (namestring logdir)))
