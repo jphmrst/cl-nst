35a5d81568261e55038ea5e910d69fbb7ce023bc
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-02-22 17:53:02 +0000

    Tag NST 1.3.1.

---
+++ tags/1.3.1/core/test-def.lisp
@@ -0,0 +1,226 @@
+;;; File test-def.lisp
+;;;
+;;; This file is part of the NST unit/regression testing system.
+;;;
+;;; Copyright (c) 2006-2009 Smart Information Flow Technologies.
+;;; Written by John Maraist.
+;;; Derived from RRT, Copyright (c) 2005 Robert Goldman.
+;;;
+;;; NST is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; NST is distributed in the hope that it will be useful, but WITHOUT
+;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+;;; Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with NST.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+(in-package :sift.nst)
+
+(defmacro def-test (name-or-name-and-args criterion &rest forms)
+  "Define a single unit test.
+
+\(def-check NAME-OR-NAME-AND-OPTIONS
+     CRITERION
+   FORM ... FORM)
+
+NAME-OR-NAME-AND-OPTIONS ::= name | NAME-AND-OPTIONS
+
+NAME-AND-OPTIONS ::= \( name [ :fixtures FORM ] [ :group GROUP ]
+                            [ :setup FORM ] [ :cleanup FORM ] )"
+
+  ;; Decode the name-or-name-and-args, pulling out the individual
+  ;; components, and indicating which are given in this test.
+  (multiple-value-bind (test-name setup setup-supp-p cleanup cleanup-supp-p
+                                  fixtures fixtures-supp-p group group-supp-p)
+      (decode-defcheck-name-and-args name-or-name-and-args)
+    (declare (ignore fixtures-supp-p))
+    (when (and group-supp-p
+               (boundp '*group-class-name*)
+               (not (eq group (symbol-value '*group-class-name*))))
+      (error "Test :group option value ~s differs from enclosing group ~s"
+             group (symbol-value '*group-class-name*)))
+    (unless (or group-supp-p (boundp '*group-class-name*))
+      (error
+       "Must specify either :group option value or enclose test in group"))
+
+    (let* ((*group-class-name*
+             (cond
+               ((boundp '*group-class-name*) (symbol-value '*group-class-name*))
+               (t group)))
+           (*group-fixture-classes*
+             (cond
+               ((boundp '*group-fixture-classes*)
+                (symbol-value '*group-fixture-classes*))
+               (t (group-fixture-class-names
+                   (make-instance *group-class-name*)))))
+           (name nil)                   ; The internal symbol used to
+                                        ; track the results of this
+                                        ; test.
+           (purge-ids nil))             ; IDs of previous record of
+                                        ; running this test.
+      (declare (special *group-class-name* *group-fixture-classes*))
+
+      ;; Find any records of running previous versions of this test.
+      (loop for report being the hash-values of +results-record+
+            using (hash-key id)
+            do
+         (when (and (eq test-name (check-result-check-name report))
+                    (eq *group-class-name* (check-result-group-name report)))
+           (push id purge-ids)))
+
+      ;; If we find one, reuse the symbol.
+      (when (eql 1 (length purge-ids))
+        (setf name (car purge-ids)))
+
+      ;; Get rid of any previous records.  For ticket:118, this would
+      ;; become setting up initial records.
+      (loop for id in purge-ids do
+        (remhash id +results-record+))
+
+      ;; If we aren't reusing a name, make up a new one.
+      (unless name
+        (setf name (gentemp (concatenate 'string
+                             (package-name (symbol-package
+                                            *group-class-name*))
+                             "-"
+                             (symbol-name *group-class-name*)
+                             "-"
+                             (package-name (symbol-package test-name))
+                             "--"
+                             (symbol-name test-name))
+                            :nst-test-class-package)))
+
+      ;; Expand the fixtures into the definitions we'll actually
+      ;; use.
+      (multiple-value-bind (fixture-class-names anon-fixture-forms
+                                                fixture-names)
+          (process-fixture-list fixtures)
+        (declare (ignorable fixture-names))
+
+        (let* ((*nst-context* nil)
+               (fixture-names-special
+                `(special ,@(loop for fx in fixture-class-names
+                                  append (bound-names fx))
+                          ,@(loop for fx in *group-fixture-classes*
+                                  append (bound-names fx))))
+               (core-run-body
+                (cond ((eql 1 (length forms))
+                       (continue-check criterion
+                         `(common-lisp:multiple-value-list
+                           (locally
+                             (declare ,fixture-names-special)
+                             ,(car forms)))))
+                      (t (continue-check criterion
+                           `(locally
+                              (declare ,fixture-names-special)
+                              (list ,@forms)))))))
+                                        ; The expansion of the actual
+                                        ; test form.
+          (declare (special *nst-context*))
+
+          `(block ,test-name
+             #+allegro (excl:record-source-file ',test-name :type :nst-test)
+             ,@anon-fixture-forms
+
+             (defclass ,name (,@fixture-class-names) ()
+                       (:metaclass singleton-class))
+
+             (finalize-inheritance (find-class ',name))
+
+             (eval-when (:load-toplevel :execute)
+               (let ((this-name-use (gethash ',test-name +name-use+)))
+                 (unless this-name-use
+                   (setf this-name-use (make-name-use)
+                         (gethash ',test-name +name-use+) this-name-use))
+                 (let ((tests-by-group (name-use-tests this-name-use)))
+                   (setf (gethash ',test-name tests-by-group)
+                         (make-instance ',name)))))
+
+             (handler-bind (#+sbcl (style-warning
+                                    #'(lambda (c)
+                                        (declare (ignore c))
+                                        ;;
+                                        ;; Uncomment to prove that
+                                        ;; SBCL does /not/ pass
+                                        ;; warnings through this
+                                        ;; handler.
+                                        ;;
+                                        ;; (format t "++++++++++ ++++++++++~%")
+                                        ;;
+                                        (muffle-warning))))
+               (defmethod group-name ((obj ,name)) ',*group-class-name*)
+               (defmethod check-user-name ((obj ,name)) ',test-name)
+               (defmethod check-group-name ((obj ,name)) ',name)
+
+               (defmethod core-run-test ((obj ,name))
+                 (declare (optimize (debug 3)) #| ,fixture-names-special |# )
+                 (let ((*current-group* ',*group-class-name*)
+                       (*current-test*  ',test-name))
+                   (declare (special *current-group* *current-test*))
+                   ,core-run-body))
+
+               ,@(when setup-supp-p
+                   `((defmethod do-test-prefixture-setup progn ((obj ,name))
+                       ,setup)))
+
+               ,@(when cleanup-supp-p
+                   `((defmethod do-test-afterfixture-cleanup progn ((obj ,name))
+                       ,cleanup))))
+
+             ;; Clear any previous stored results, since we've just
+             ;; (re-)defined this check.
+             #|(when (boundp '+results-record+)
+                 (remhash ',suite-class-name
+                          (symbol-value '+results-record+)))|#
+
+             ;; Provide debugging information about this test.
+             (defmethod trace-test ((gr ,*group-class-name*)
+                                    (ts ,name))
+               (format t "Test ~s (group ~s)~%" gr ts)
+               (format t " - Given name and args: ~s~%" ',name-or-name-and-args)
+               (format t " - Given criterion: ~s~%" ',criterion)
+               (format t " - Given forms: ~@<~{~s~^ ~:_~}~:>~%" ',forms))
+
+             ;; Pretty printer.
+             (set-pprint-dispatch ',name
+               #'(lambda (stream object)
+                   (declare (ignorable object))
+                   (format stream
+                       ,(format nil "Test ~s of group ~s"
+                          test-name
+                          *group-class-name*))))
+
+             ;; Pass the test record predicate.
+             (defmethod test-record-p ((obj ,name)) t)
+
+             (defmethod test-name-lookup ((ts ,name)) ',test-name)
+
+             ;; Store the new artifact against the uses of its
+             ;; name in NST.
+             (note-executable ',test-name (make-instance ',name))
+
+             (let ((gproto (make-instance ',*group-class-name*)))
+               (setf (test-list gproto)
+                     (nconc (test-list gproto) (list ',name)))
+               (setf (gethash ',test-name (test-name-lookup gproto))
+                     (make-instance ',name)))))))))
+
+(defpackage nst-test-class-package
+    (:documentation "Internal package for NST tests' class names."))
+
+(defmacro def-check (&rest args)
+  (warn 'nst-soft-deprecation :old-name 'def-check :replacement 'def-test)
+  `(def-test ,@args))
+
+(defmacro debug-check (defcheck)
+  "Debugging aid for def-check forms.  Provides all-caps dummy values for
+dynamic variables normally provided by def-test-group."
+  `(let ((*group-fixture-classes* '(<<GROUP-FIXTURE-CLASSES>>))
+         (*group-class-name* '<<GROUP-CLASS-NAME>>))
+     (declare (special *group-class-name* *group-fixture-classes*))
+     (pprint (macroexpand ',defcheck))))
---
+++ tags/1.3.1/platforms/batch-runs.bash
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+./setup-inputs.bash
+mkdir -p logs
+
+# echo Testing the branch on ECL
+# echo > logs/ecl-branch.log
+# ecl -load branch-pushes.lisp 2>> logs/ecl-branch.log >> logs/ecl-branch.log
+# grep TOTAL logs/ecl-branch.log | tail -1
+
+# echo Testing the trunk on ECL
+# echo > logs/ecl-trunk.log
+# ecl -load trunk-pushes.lisp 2>> logs/ecl-trunk.log >> logs/ecl-trunk.log
+# grep TOTAL logs/ecl-trunk.log | tail -1
+
+# ------------------------------------------------------------
+
+
+# Clear out CLISP FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/clisp/*/*
+
+echo Testing the trunk \(with everything recompiled\) on CLISP
+echo > logs/clisp-trunk.log
+clisp -i inputs/clisp/init.lisp inputs/clisp/nst-tests.lisp \
+    2>> logs/clisp-trunk.log >> logs/clisp-trunk.log
+grep TOTAL logs/clisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on CLISP
+mkdir -p clisp-junit
+rm -f clisp-junit/*
+export NSTJUNITDIR=clisp-junit/
+clisp -i inputs/clisp/init.lisp inputs/clisp/junit.lisp \
+    2>> clisp-junit/clisp-trunk.log >> clisp-junit/clisp-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+# Clear out Clozure FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/misc/ccl-*/*
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(with everything recompiled\) on Clozure
+echo > logs/clozure-trunk.log
+~/Lib/Lisp/clozure/ccl/lx86cl --batch < inputs/clozure/nst-tests.lisp \
+    2>> logs/clozure-trunk.log >> logs/clozure-trunk.log
+grep TOTAL logs/clozure-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Clozure
+mkdir -p clozure-junit
+rm -f clozure-junit/*
+export NSTJUNITDIR=clozure-junit/
+~/Lib/Lisp/clozure/ccl/lx86cl --batch < inputs/clozure/junit.lisp \
+    2>> clozure-junit/junit.log >> clozure-junit/junit.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+# Clear out SBCL FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/sbcl/*/*
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(with everything recompiled\) on SBCL
+echo > logs/sbcl-trunk.log
+sbcl < inputs/sbcl/nst-tests.lisp \
+    2>> logs/sbcl-trunk.log >> logs/sbcl-trunk.log
+grep TOTAL logs/sbcl-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on SBCL
+mkdir -p sbcl-junit
+rm -f sbcl-junit/*
+export NSTJUNITDIR=sbcl-junit/
+sbcl < inputs/sbcl/junit.lisp \
+    2>> sbcl-junit/sbcl-trunk.log >> sbcl-junit/sbcl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+ 
+# echo Testing the trunk on CMU CL
+# echo > logs/cmucl-trunk.log
+# cmucl -batch < trunk-asdfind.lisp 2>> logs/cmucl-trunk.log >> logs/cmucl-trunk.log
+# grep TOTAL logs/cmucl-trunk.log | tail -1
+
+
+
+# Clear out Allegro FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/allegro/*/*
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(with everything recompiled\) on Allegro 8.2 mlisp
+echo > logs/allegro82-mlisp-trunk.log
+mlisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro82-mlisp-trunk.log >> logs/allegro82-mlisp-trunk.log
+grep TOTAL logs/allegro82-mlisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.2 mlisp
+mkdir -p acl82-mlisp-junit
+rm -f acl82-mlisp-junit/*
+export NSTJUNITDIR=acl82-mlisp-junit/
+mlisp < inputs/acl/junit.lisp \
+    2>> acl82-mlisp-junit/acl-trunk.log >> acl82-mlisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(with everything recompiled\) on Allegro 8.2 alisp
+echo > logs/allegro82-alisp-trunk.log
+alisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro82-alisp-trunk.log >> logs/allegro82-alisp-trunk.log
+grep TOTAL logs/allegro82-alisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.2 alisp
+mkdir -p acl82-alisp-junit
+rm -f acl82-alisp-junit/*
+export NSTJUNITDIR=acl82-alisp-junit/
+alisp < inputs/acl/junit.lisp \
+    2>> acl82-alisp-junit/acl-trunk.log >> acl82-alisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(with everything recompiled\) on Allegro 8.1 mlisp
+echo > logs/allegro81-mlisp-trunk.log
+mlisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro81-mlisp-trunk.log >> logs/allegro81-mlisp-trunk.log
+grep TOTAL logs/allegro81-mlisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.1 mlisp
+mkdir -p acl81-mlisp-junit
+rm -f acl81-mlisp-junit/*
+export NSTJUNITDIR=acl81-mlisp-junit/
+/usr/local/allegro/acl81/mlisp < inputs/acl/junit.lisp \
+    2>> acl81-mlisp-junit/acl-trunk.log >> acl81-mlisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(with everything recompiled\) on Allegro 8.1 alisp
+echo > logs/allegro81-alisp-trunk.log
+alisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro81-alisp-trunk.log >> logs/allegro81-alisp-trunk.log
+grep TOTAL logs/allegro81-alisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.1 alisp
+mkdir -p acl81-alisp-junit
+rm -f acl81-alisp-junit/*
+export NSTJUNITDIR=acl81-alisp-junit/
+/usr/local/allegro/acl81/alisp < inputs/acl/junit.lisp \
+    2>> acl81-alisp-junit/acl-trunk.log >> acl81-alisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
