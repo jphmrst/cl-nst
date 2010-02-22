9a84a4dd05ab4631bdb033c493627909cfd26078
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-02-22 17:22:39 +0000

    Test more versions/modes of ACL, and clear out old FASLs before doing so.

--- platforms/batch-runs.bash
+++ platforms/batch-runs.bash
@@ -13,6 +13,12 @@ mkdir -p logs
 # ecl -load trunk-pushes.lisp 2>> logs/ecl-trunk.log >> logs/ecl-trunk.log
 # grep TOTAL logs/ecl-trunk.log | tail -1
 
+# ------------------------------------------------------------
+
+
+# Clear out CLISP FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/clisp/*/*
+
 echo Testing the trunk on CLISP
 echo > logs/clisp-trunk.log
 clisp -i inputs/clisp/init.lisp inputs/clisp/nst-tests.lisp \
@@ -30,6 +36,9 @@ echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
 
+
+# Clear out Clozure FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/misc/ccl-*/*
 
 echo ------------------------------------------------------------
 echo Testing the trunk on Clozure
@@ -49,6 +58,8 @@ echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
 
+# Clear out SBCL FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/sbcl/*/*
 
 echo ------------------------------------------------------------
 echo Testing the trunk on SBCL
@@ -76,19 +87,79 @@ echo Files in $NSTJUNITDIR directory: expected 15, have \
 
 
 
-echo ------------------------------------------------------------
-echo Testing the trunk on Allegro
-echo > logs/allegro-trunk.log
-mlisp < inputs/acl/nst-tests.lisp \
-    2>> logs/allegro-trunk.log >> logs/allegro-trunk.log
-grep TOTAL logs/allegro-trunk.log | tail -1
+# Clear out Allegro FASLs
+rm -rf /home/jm/Lib/Lisp/fasl/allegro/*/*
 
 echo ------------------------------------------------------------
-echo Testing JUnit writing on Allegro CL
-mkdir -p acl-junit
-rm -f acl-junit/*
-export NSTJUNITDIR=acl-junit/
+echo Testing the trunk on Allegro 8.2 mlisp
+echo > logs/allegro82-mlisp-trunk.log
+mlisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro82-mlisp-trunk.log >> logs/allegro82-mlisp-trunk.log
+grep TOTAL logs/allegro82-mlisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.2 mlisp
+mkdir -p acl82-mlisp-junit
+rm -f acl82-mlisp-junit/*
+export NSTJUNITDIR=acl82-mlisp-junit/
 mlisp < inputs/acl/junit.lisp \
-    2>> acl-junit/acl-trunk.log >> acl-junit/acl-trunk.log
+    2>> acl82-mlisp-junit/acl-trunk.log >> acl82-mlisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+echo ------------------------------------------------------------
+echo Testing the trunk on Allegro 8.2 alisp
+echo > logs/allegro82-alisp-trunk.log
+alisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro82-alisp-trunk.log >> logs/allegro82-alisp-trunk.log
+grep TOTAL logs/allegro82-alisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.2 alisp
+mkdir -p acl82-alisp-junit
+rm -f acl82-alisp-junit/*
+export NSTJUNITDIR=acl82-alisp-junit/
+alisp < inputs/acl/junit.lisp \
+    2>> acl82-alisp-junit/acl-trunk.log >> acl82-alisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+echo ------------------------------------------------------------
+echo Testing the trunk on Allegro 8.1 mlisp
+echo > logs/allegro81-mlisp-trunk.log
+mlisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro81-mlisp-trunk.log >> logs/allegro81-mlisp-trunk.log
+grep TOTAL logs/allegro81-mlisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.1 mlisp
+mkdir -p acl81-mlisp-junit
+rm -f acl81-mlisp-junit/*
+export NSTJUNITDIR=acl81-mlisp-junit/
+/usr/local/allegro/acl81/mlisp < inputs/acl/junit.lisp \
+    2>> acl81-mlisp-junit/acl-trunk.log >> acl81-mlisp-junit/acl-trunk.log
+echo Files in $NSTJUNITDIR directory: expected 15, have \
+    `ls -1 $NSTJUNITDIR | wc -l`
+
+
+
+echo ------------------------------------------------------------
+echo Testing the trunk on Allegro 8.1 alisp
+echo > logs/allegro81-alisp-trunk.log
+alisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro81-alisp-trunk.log >> logs/allegro81-alisp-trunk.log
+grep TOTAL logs/allegro81-alisp-trunk.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing JUnit writing on Allegro 8.1 alisp
+mkdir -p acl81-alisp-junit
+rm -f acl81-alisp-junit/*
+export NSTJUNITDIR=acl81-alisp-junit/
+/usr/local/allegro/acl81/alisp < inputs/acl/junit.lisp \
+    2>> acl81-alisp-junit/acl-trunk.log >> acl81-alisp-junit/acl-trunk.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
