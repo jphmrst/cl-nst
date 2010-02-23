751eb24364ea8349761d2885fe46ab86c67e8ce5
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-02-23 15:55:39 +0000

    One change from yesterday --- for the batch runs of multiple platforms, run everything both with a resh compile, and by pulling the compiled FASLs.  In the past, there have been problems with one but not the other, so will try both.

--- trunk/platforms/batch-runs.bash
+++ trunk/platforms/batch-runs.bash
@@ -9,9 +9,9 @@ mkdir -p logs
 # grep TOTAL logs/ecl-branch.log | tail -1
 
 # echo Testing the trunk on ECL
-# echo > logs/ecl-trunk.log
-# ecl -load trunk-pushes.lisp 2>> logs/ecl-trunk.log >> logs/ecl-trunk.log
-# grep TOTAL logs/ecl-trunk.log | tail -1
+# echo > logs/ecl-fresh.log
+# ecl -load trunk-pushes.lisp 2>> logs/ecl-fresh.log >> logs/ecl-fresh.log
+# grep TOTAL logs/ecl-fresh.log | tail -1
 
 # ------------------------------------------------------------
 
@@ -20,10 +20,17 @@ mkdir -p logs
 rm -rf /home/jm/Lib/Lisp/fasl/clisp/*/*
 
 echo Testing the trunk \(with everything recompiled\) on CLISP
-echo > logs/clisp-trunk.log
+echo > logs/clisp-fresh.log
 clisp -i inputs/clisp/init.lisp inputs/clisp/nst-tests.lisp \
-    2>> logs/clisp-trunk.log >> logs/clisp-trunk.log
-grep TOTAL logs/clisp-trunk.log | tail -1
+    2>> logs/clisp-fresh.log >> logs/clisp-fresh.log
+grep TOTAL logs/clisp-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on CLISP
+echo > logs/clisp-fresh.log
+clisp -i inputs/clisp/init.lisp inputs/clisp/nst-tests.lisp \
+    2>> logs/clisp-fresh.log >> logs/clisp-fresh.log
+grep TOTAL logs/clisp-fresh.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on CLISP
@@ -31,7 +38,7 @@ mkdir -p clisp-junit
 rm -f clisp-junit/*
 export NSTJUNITDIR=clisp-junit/
 clisp -i inputs/clisp/init.lisp inputs/clisp/junit.lisp \
-    2>> clisp-junit/clisp-trunk.log >> clisp-junit/clisp-trunk.log
+    2>> clisp-junit/junit.log >> clisp-junit/junit.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
@@ -42,10 +49,17 @@ rm -rf /home/jm/Lib/Lisp/fasl/misc/ccl-*/*
 
 echo ------------------------------------------------------------
 echo Testing the trunk \(with everything recompiled\) on Clozure
-echo > logs/clozure-trunk.log
+echo > logs/clozure-fresh.log
 ~/Lib/Lisp/clozure/ccl/lx86cl --batch < inputs/clozure/nst-tests.lisp \
-    2>> logs/clozure-trunk.log >> logs/clozure-trunk.log
-grep TOTAL logs/clozure-trunk.log | tail -1
+    2>> logs/clozure-fresh.log >> logs/clozure-fresh.log
+grep TOTAL logs/clozure-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on Clozure
+echo > logs/clozure-reload.log
+~/Lib/Lisp/clozure/ccl/lx86cl --batch < inputs/clozure/nst-tests.lisp \
+    2>> logs/clozure-reload.log >> logs/clozure-reload.log
+grep TOTAL logs/clozure-reload.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on Clozure
@@ -63,10 +77,17 @@ rm -rf /home/jm/Lib/Lisp/fasl/sbcl/*/*
 
 echo ------------------------------------------------------------
 echo Testing the trunk \(with everything recompiled\) on SBCL
-echo > logs/sbcl-trunk.log
+echo > logs/sbcl-fresh.log
 sbcl < inputs/sbcl/nst-tests.lisp \
-    2>> logs/sbcl-trunk.log >> logs/sbcl-trunk.log
-grep TOTAL logs/sbcl-trunk.log | tail -1
+    2>> logs/sbcl-fresh.log >> logs/sbcl-fresh.log
+grep TOTAL logs/sbcl-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on SBCL
+echo > logs/sbcl-reload.log
+sbcl < inputs/sbcl/nst-tests.lisp \
+    2>> logs/sbcl-reload.log >> logs/sbcl-reload.log
+grep TOTAL logs/sbcl-reload.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on SBCL
@@ -74,16 +95,16 @@ mkdir -p sbcl-junit
 rm -f sbcl-junit/*
 export NSTJUNITDIR=sbcl-junit/
 sbcl < inputs/sbcl/junit.lisp \
-    2>> sbcl-junit/sbcl-trunk.log >> sbcl-junit/sbcl-trunk.log
+    2>> sbcl-junit/junit.log >> sbcl-junit/junit.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
 
  
 # echo Testing the trunk on CMU CL
-# echo > logs/cmucl-trunk.log
-# cmucl -batch < trunk-asdfind.lisp 2>> logs/cmucl-trunk.log >> logs/cmucl-trunk.log
-# grep TOTAL logs/cmucl-trunk.log | tail -1
+# echo > logs/cmucl-fresh.log
+# cmucl -batch < trunk-asdfind.lisp 2>> logs/cmucl-fresh.log >> logs/cmucl-fresh.log
+# grep TOTAL logs/cmucl-fresh.log | tail -1
 
 
 
@@ -92,10 +113,17 @@ rm -rf /home/jm/Lib/Lisp/fasl/allegro/*/*
 
 echo ------------------------------------------------------------
 echo Testing the trunk \(with everything recompiled\) on Allegro 8.2 mlisp
-echo > logs/allegro82-mlisp-trunk.log
+echo > logs/allegro82-mlisp-fresh.log
 mlisp < inputs/acl/nst-tests.lisp \
-    2>> logs/allegro82-mlisp-trunk.log >> logs/allegro82-mlisp-trunk.log
-grep TOTAL logs/allegro82-mlisp-trunk.log | tail -1
+    2>> logs/allegro82-mlisp-fresh.log >> logs/allegro82-mlisp-fresh.log
+grep TOTAL logs/allegro82-mlisp-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on Allegro 8.2 mlisp
+echo > logs/allegro82-mlisp-reload.log
+mlisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro82-mlisp-reload.log >> logs/allegro82-mlisp-reload.log
+grep TOTAL logs/allegro82-mlisp-reload.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on Allegro 8.2 mlisp
@@ -103,7 +131,7 @@ mkdir -p acl82-mlisp-junit
 rm -f acl82-mlisp-junit/*
 export NSTJUNITDIR=acl82-mlisp-junit/
 mlisp < inputs/acl/junit.lisp \
-    2>> acl82-mlisp-junit/acl-trunk.log >> acl82-mlisp-junit/acl-trunk.log
+    2>> acl82-mlisp-junit/junit.log >> acl82-mlisp-junit/junit.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
@@ -111,10 +139,17 @@ echo Files in $NSTJUNITDIR directory: expected 15, have \
 
 echo ------------------------------------------------------------
 echo Testing the trunk \(with everything recompiled\) on Allegro 8.2 alisp
-echo > logs/allegro82-alisp-trunk.log
+echo > logs/allegro82-alisp-fresh.log
 alisp < inputs/acl/nst-tests.lisp \
-    2>> logs/allegro82-alisp-trunk.log >> logs/allegro82-alisp-trunk.log
-grep TOTAL logs/allegro82-alisp-trunk.log | tail -1
+    2>> logs/allegro82-alisp-fresh.log >> logs/allegro82-alisp-fresh.log
+grep TOTAL logs/allegro82-alisp-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on Allegro 8.2 alisp
+echo > logs/allegro82-alisp-reload.log
+alisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro82-alisp-reload.log >> logs/allegro82-alisp-reload.log
+grep TOTAL logs/allegro82-alisp-reload.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on Allegro 8.2 alisp
@@ -122,7 +157,7 @@ mkdir -p acl82-alisp-junit
 rm -f acl82-alisp-junit/*
 export NSTJUNITDIR=acl82-alisp-junit/
 alisp < inputs/acl/junit.lisp \
-    2>> acl82-alisp-junit/acl-trunk.log >> acl82-alisp-junit/acl-trunk.log
+    2>> acl82-alisp-junit/junit.log >> acl82-alisp-junit/junit.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
@@ -130,10 +165,17 @@ echo Files in $NSTJUNITDIR directory: expected 15, have \
 
 echo ------------------------------------------------------------
 echo Testing the trunk \(with everything recompiled\) on Allegro 8.1 mlisp
-echo > logs/allegro81-mlisp-trunk.log
+echo > logs/allegro81-mlisp-fresh.log
 mlisp < inputs/acl/nst-tests.lisp \
-    2>> logs/allegro81-mlisp-trunk.log >> logs/allegro81-mlisp-trunk.log
-grep TOTAL logs/allegro81-mlisp-trunk.log | tail -1
+    2>> logs/allegro81-mlisp-fresh.log >> logs/allegro81-mlisp-fresh.log
+grep TOTAL logs/allegro81-mlisp-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on Allegro 8.1 mlisp
+echo > logs/allegro81-mlisp-reload.log
+mlisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro81-mlisp-reload.log >> logs/allegro81-mlisp-reload.log
+grep TOTAL logs/allegro81-mlisp-reload.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on Allegro 8.1 mlisp
@@ -141,7 +183,7 @@ mkdir -p acl81-mlisp-junit
 rm -f acl81-mlisp-junit/*
 export NSTJUNITDIR=acl81-mlisp-junit/
 /usr/local/allegro/acl81/mlisp < inputs/acl/junit.lisp \
-    2>> acl81-mlisp-junit/acl-trunk.log >> acl81-mlisp-junit/acl-trunk.log
+    2>> acl81-mlisp-junit/junit.log >> acl81-mlisp-junit/junit.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
 
@@ -149,10 +191,17 @@ echo Files in $NSTJUNITDIR directory: expected 15, have \
 
 echo ------------------------------------------------------------
 echo Testing the trunk \(with everything recompiled\) on Allegro 8.1 alisp
-echo > logs/allegro81-alisp-trunk.log
+echo > logs/allegro81-alisp-fresh.log
 alisp < inputs/acl/nst-tests.lisp \
-    2>> logs/allegro81-alisp-trunk.log >> logs/allegro81-alisp-trunk.log
-grep TOTAL logs/allegro81-alisp-trunk.log | tail -1
+    2>> logs/allegro81-alisp-fresh.log >> logs/allegro81-alisp-fresh.log
+grep TOTAL logs/allegro81-alisp-fresh.log | tail -1
+
+echo ------------------------------------------------------------
+echo Testing the trunk \(without recompiling\) on Allegro 8.1 alisp
+echo > logs/allegro81-alisp-reload.log
+alisp < inputs/acl/nst-tests.lisp \
+    2>> logs/allegro81-alisp-reload.log >> logs/allegro81-alisp-reload.log
+grep TOTAL logs/allegro81-alisp-reload.log | tail -1
 
 echo ------------------------------------------------------------
 echo Testing JUnit writing on Allegro 8.1 alisp
@@ -160,6 +209,6 @@ mkdir -p acl81-alisp-junit
 rm -f acl81-alisp-junit/*
 export NSTJUNITDIR=acl81-alisp-junit/
 /usr/local/allegro/acl81/alisp < inputs/acl/junit.lisp \
-    2>> acl81-alisp-junit/acl-trunk.log >> acl81-alisp-junit/acl-trunk.log
+    2>> acl81-alisp-junit/junit.log >> acl81-alisp-junit/junit.log
 echo Files in $NSTJUNITDIR directory: expected 15, have \
     `ls -1 $NSTJUNITDIR | wc -l`
