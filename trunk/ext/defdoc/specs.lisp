cbf7ca308c395fde5ce91f22efe4bcde70421eeb
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-04-04 17:32:20 +0000

    Add a directory for def-doc.

---
+++ trunk/ext/defdoc/specs.lisp
@@ -0,0 +1,8 @@
+
+(defun deconstruct-spec (name-or-spec)
+  (cond
+   ((symbolp name-or-spec)
+    (values name-or-spec nil))
+   ((listp name-or-spec)
+    (destructuring-bind (name &key method) name-or-spec
+      (values name method)))))
