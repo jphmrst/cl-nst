5db81653e6f79af9ce4082f4bff7171b6235662c
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-05-31 15:31:18 +0000

    checking that my login here is cleared up

--- trunk/ext/defdoc/macro.lisp
+++ trunk/ext/defdoc/macro.lisp
@@ -1,11 +1,38 @@
+(in-package :defdoc)
 
 (defmacro def-documentation (name-or-spec &key (intro nil intro-supp-p)
                                           (params nil params-supp-p)
                                           (full nil full-supp-p))
-  (multiple-value-bind (name method-spec) name-or-spec
-  `(progn
-     ,@(cond
-         (intro-supp-p `((setf (documentation ',name) ,intro)))
-         )
-     )))
+  "Doc doc doc"
+   (multiple-value-bind (name method-spec) (deconstruct-spec name-or-spec)
+     `(let (doc-symbol)
+
+        (cond
+          ((macro-function ',name)
+           (setf doc-symbol 'compiler-macro))
+          ((block check-fn
+             (handler-bind ((undefined-function
+                             #'(lambda (e) (return-from check-fn nil))))
+               (symbol-function ',name)))
+           (setf doc-symbol 'function)))
+
+        (let ((plain-string ,(cond
+                               (intro-supp-p
+                                (cond
+                                  ((stringp intro) intro)
+                                  ((symbolp intro) (symbol-name intro))
+                                  (t "")
+                                  ))
+                               (t ""))))
+          )
+        ,@(cond
+           (intro-supp-p `((setf (documentation ',name) ,intro)))
+           )
+        )))
+
+(defgeneric f (d)
+  (:documentation "Blah blah"))
+
+(defmethod f ((d symbol))
+  "Bleh bleh")
 
