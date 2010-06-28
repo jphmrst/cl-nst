7aaefcba9f9b54eecc436ddbf76a510ad17fed2a
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-06-28 17:44:55 +0000

    Some tests I'd started to add yesterday

--- trunk/test/meta/redefs.lisp
+++ trunk/test/meta/redefs.lisp
@@ -10,5 +10,18 @@
        (eval '(def-test-group ,group-name () (def-test ,test-name (:eql 5) 5)))
        (--nst-group ,group-name (---test-passes ,group-name ,test-name)))))
 
+(def-criterion-alias (:redefN first-tests second-tests)
+  (let ((group-name (gentemp)))
+    `(:progn
+       (eval '(def-test-group ,group-name ()
+                ,@(loop for name in first-tests collect `(def-test ,name (:eql 6) 5))))
+       (run-group ',group-name)
+       (eval '(def-test-group ,group-name ()
+                ,@(loop for name in second-tests collect `(def-test ,name (:eql 5) 5))))
+       (:all ,@(loop for name in second-tests
+                     collect `(--nst-group ,group-name (---test-passes ,group-name ,name)))
+             ))))
+
 (def-test-group redefs ()
-  (def-test redef :redef))
+  (def-test redef0 :redef)
+  (def-test redef1 (:redefN (aa bb cc) (xx yy))))
