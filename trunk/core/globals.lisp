16dac3f22e40c0c3293197b84cb486cb3df4b000
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-06-29 13:53:48 +0000

    update cpoyright year

--- trunk/core/globals.lisp
+++ trunk/core/globals.lisp
@@ -2,7 +2,7 @@
 ;;;
 ;;; This file is part of the NST unit/regression testing system.
 ;;;
-;;; Copyright (c) 2006-2009 Smart Information Flow Technologies.
+;;; Copyright (c) 2006-2010 Smart Information Flow Technologies.
 ;;; Written by John Maraist.
 ;;; Derived from RRT, Copyright (c) 2005 Robert Goldman.
 ;;;
