94e665a43cdb7f4243c306f903d7abc836b713b8
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-09-01 02:56:36 +0000

    checkpoint (this code not used, not included in release tarballs)

---
+++ ext/defdoc/example/package.lisp
@@ -0,0 +1,32 @@
+;;; File package.lisp
+;;;
+;;; This file is part of the NST unit/regression testing system.
+;;;
+;;; Copyright (c) 2010 Smart Information Flow Technologies.
+;;; Written by John Maraist.
+;;;
+;;; NST is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; NST is distributed in the hope that it will be useful, but WITHOUT
+;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+;;; Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with NST.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+(in-package :common-lisp-user)
+
+(defpackage :defdoc-ex
+    (:documentation "Structured documentation definition examples")
+    (:nicknames :ddocex))
+
+(defun defdoc-ex::make-package-documentation ()
+  "Write documentation for this package, using system package-doc."
+  (asdf:oos 'asdf:load-op 'package-doc)
+  (funcall (symbol-function (intern (symbol-name 'package-doc)
+                                    (find-package :package-doc)))
+           (find-package :defdoc)))
