522b1f1797c445eb3c22d6372d020b0952df9fe4
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-09-28 03:52:47 +0000

    docs generation works on acl and sbcl; clisp, clozure, lispworks have problems

--- doc/doc.lisp
+++ doc/doc.lisp
@@ -22,12 +22,12 @@
 
 (defun build-nst-docs ()
   "Write documentation for this package, using system package-doc."
-  (format t "*load-truename* ~s~%" *load-truename*)
   (let* ((doc-root-dir (asdf:system-relative-pathname (asdf:find-system :nst)
                                                       "doc/"))
          (gen-dir (merge-pathnames #p"gen/" doc-root-dir))
          (manual-dir (merge-pathnames #p"manual/" doc-root-dir))
          (quickref-dir (merge-pathnames #p"quickref/" doc-root-dir)))
+    (format t "Creating documentation in ~a~%" doc-root-dir)
     (defdoc:write-doctype-latex 'nst::criterion
         :echo #'(lambda (&key name type)
                   (format t "Writing ~a ~a...~%" type name))
@@ -75,20 +75,53 @@
 ;;;                (setf (current-directory) manual-dir)
 ;;;                (run-program "pdflatex" '("manual.tex")
 ;;;                             :wait t :search t :output nil :error t)
-;;;             (sb-ext:run-program "makeindex" '("manual")
-;;;                                 :wait t :search t :output nil :error t)
+;;;                (run-program "makeindex" '("manual")
+;;;                             :wait t :search t :output nil :error t)
 ;;;                (run-program "pdflatex" '("manual.tex")
-;;;                             :wait t :search t :output nil :error t))
+;;;                             :wait t :search t :output nil :error t)
 ;;;                (run-program "pdflatex" '("manual.tex")
+;;;                             :wait t :search t :output nil :error t)
+;;;                (setf (current-directory) quickref-dir)
+;;;                (run-program "pdflatex" '("quickref.tex")
 ;;;                             :wait t :search t :output nil :error t))
 
-    #-(or allegro sbcl ;; clozure
+;;;    #+clisp (progn
+;;;              (ext:cd manual-dir)
+;;;              (ext:shell "pdflatex manual.tex")
+;;;              (ext:shell "makeindex manual")
+;;;              (ext:shell "pdflatex manual.tex")
+;;;              (ext:shell "pdflatex manual.tex")
+;;;              (ext:cd quickref-dir)
+;;;              (ext:shell "pdflatex quickref.tex"))
+
+;;;    #+lispworks (progn
+;;;                  (hcl:change-directory manual-dir)
+;;;                  (system:call-system "pdflatex manual.tex")
+;;;                  (system:call-system "makeindex manual")
+;;;                  (system:call-system "pdflatex manual.tex")
+;;;                  (system:call-system "pdflatex manual.tex")
+;;;                  (hcl:change-directory quickref-dir)
+;;;                  (system:call-system "pdflatex quickref.tex"))
+
+    #-(or allegro sbcl ;; clozure clisp lispworks
           )
     (warn "Documentation building not fully implemented on this system --- please run~%  pdflatex manual.tex~%  makeindex manual~%  pdflatex manual.tex~%from the directory~%  ~a~%" manual-dir)))
 
 (defclass nst-criterion-style (defdoc:package-list-latex-style) ())
+(defmethod defdoc:get-latex-output-file-name ((style nst-criterion-style)
+                                              usage name)
+  (string-downcase (concatenate 'string
+                     (symbol-name name) "_"
+                     (symbol-name usage) "_"
+                     (symbol-name (type-of style)) ".tex")))
 
 (defclass nst-item-style (defdoc:latex-style) ())
+(defmethod defdoc:get-latex-output-file-name ((style nst-item-style)
+                                              usage name)
+  (string-downcase (concatenate 'string
+                     (symbol-name name) "_"
+                     (symbol-name usage) "_"
+                     (symbol-name (type-of style)) ".tex")))
 (defmethod defdoc:latex-style-adjust-spec-element ((style nst-item-style)
                                                    spec-type spec-head
                                                    (element (eql :intro))
@@ -107,6 +140,12 @@
          ,(call-next-method))))
 
 (defclass nst-quickref (defdoc:latex-style) ())
+(defmethod defdoc:get-latex-output-file-name ((style nst-quickref)
+                                              usage name)
+  (string-downcase (concatenate 'string
+                     (symbol-name name) "_"
+                     (symbol-name usage) "_"
+                     (symbol-name (type-of style)) ".tex")))
 (defmethod format-docspec-element ((style nst-quickref) spec-type
                                    (in (eql :spec)) stream spec-args)
   (destructuring-bind (&key self
@@ -146,7 +185,6 @@
                               :subspec spec-args subspec)
                         spec-type))
       (princ "\\end{description}" stream))))
-
 
 (defclass nst-package-list-latex-style (defdoc:package-list-latex-style) ())
 (defmethod defdoc:package-list-entry ((style nst-package-list-latex-style)
