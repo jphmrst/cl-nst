de3ad618275cacf6f6515eb40d48acaff69aa849
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-10-21 17:51:12 +0000

    Tweak the verbosity settings.

--- trunk/ext/defdoc/globals.lisp
+++ trunk/ext/defdoc/globals.lisp
@@ -4,3 +4,13 @@
 (defvar *latex-verbatim-width* 65)
 (defvar *latex-full-package-item-header-macro* "\\paragraph")
 (defvar *defdoc-latex-default-directory* #p"./")
+
+;;;(defvar *default-spec-compiler* #'standard-spec-compiler)
+;;;
+;;;(defgeneric get-spec-compiler (name usage spec-head spec-args)
+;;;  (:documentation "Get the function which should compile a particular spec")
+;;;
+;;;  (:method (name usage spec-head spec-args)
+;;;     (declare (ignore name usage spec-head spec-args))
+;;;     *default-spec-compiler*))
+
