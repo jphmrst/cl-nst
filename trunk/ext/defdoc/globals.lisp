f692f0a0aa22e3ff405a4673c98a1e1161ce2303
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2010-10-23 17:35:32 +0000

    consolidate some too-small source files

--- trunk/ext/defdoc/globals.lisp
+++ trunk/ext/defdoc/globals.lisp
@@ -1,4 +1,17 @@
 (in-package :defdoc)
 
 (defvar *docstring-style* 'standard-docstring-style)
-(defvar *latex-verbatim-width* 65)
+
+(defun format-doc (stream style spec)
+  (let ((target-type (docspec-target-type spec)))
+    (format-docspec stream style spec target-type)))
+
+(defgeneric format-docspec (stream style spec type)
+  (:method :around (stream style spec type)
+     (cond ((symbolp style)
+            (format-docspec stream (make-instance style) spec type))
+           (t (call-next-method))))
+  (:method (stream style spec type)
+     (format-docspec-element style type spec stream)))
+
+(defgeneric format-docspec-element (style target-type spec stream))
