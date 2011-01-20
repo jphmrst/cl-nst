87ce34a539b3c9c47688b9915b5bc200f6aecb61
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2011-01-20 05:00:28 +0000

    add non-compiled tests

--- test/direct/package.lisp
+++ test/direct/package.lisp
@@ -3,3 +3,7 @@
     (:documentation "Package for reflective NST test suites")
     (:use :common-lisp :nst :nst-test-utils))
 
+(defpackage :nst-simple-tests-interpreted
+    (:documentation "Package for interpreted, reflective NST test suites")
+    (:use :common-lisp :nst :nst-test-utils))
+
