615fc6710eed1011c1785531065e915de441a0cc
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2011-10-12 21:58:04 +0000

    Make sure that the NST ASDF is of a type known to throw a failure bomb if a test fails. This is a crude way to take care of the false-positives we saw in SHOP2 when I put the wrong ASDF system as the one to be sent to the test-op.

--- jenkins/NSTjenkins/System.pm
+++ jenkins/NSTjenkins/System.pm
@@ -18,13 +18,21 @@ sub initialize {
       ('description', 'Abstract superclass of an ASDF system invocation.');
   $self->declare_scalar_variable('name', $name);
   $self->declare_scalar_variable('force', undef);
+  $self->declare_scalar_variable('norequirefailflag', undef);
 }
 
 sub invocation {
   my $self = shift;
+
   my $result = "(asdf:oos 'asdf:test-op :" . $self->name;
   $result .= " :force t" if ($self->force);
-  $result .= ")";
+  $result .= ")\n";
+
+  if (!$self->norequirefailflag()) {
+    $result .= '(let ((sys (asdf:find-system :'
+        . $self->name . '))) (unless (and (find-package :sift.asdf-nst) (typep sys \'sift.asdf-nst::nst-test-holder) (sift.asdf-nst::error-when-nst sys)) (error "System ~s not flagged to raise error on test failure --- may cause false positive" sys)))';
+  }
+
   return $result;
 }
 
