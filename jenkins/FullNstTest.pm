75ce620b19f446dc41e16369ebd78c63c10c0afb
jmaraist <jmaraist@94a9f931-981c-1dd3-1dc1-8722fd5794fd>
2011-10-12 04:32:25 +0000

    Also test Allegro 8-bit mode

--- jenkins/FullNstTest.pm
+++ jenkins/FullNstTest.pm
@@ -57,8 +57,11 @@ sub initialize {
   $self->add_platform(NSTjenkins::CLISP::instance());
   $self->add_platform(NSTjenkins::CLISP::instance()->mixedcase(1));
   $self->add_platform(NSTjenkins::SBCL::instance());
-  $self->add_platform(NSTjenkins::Allegro::instance()->mixedcase(0));
+  $self->add_platform(NSTjenkins::Allegro::instance());
   $self->add_platform(NSTjenkins::Allegro::instance()->mixedcase(1));
+  $self->add_platform(NSTjenkins::Allegro::instance()->eightbitmode(1));
+  $self->add_platform
+      (NSTjenkins::Allegro::instance()->mixedcase(1)->eightbitmode(1));
 }
 
 1;
